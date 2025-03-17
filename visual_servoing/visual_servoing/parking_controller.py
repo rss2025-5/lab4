706eb248cff4ea53a53eb5b3756ba8aed8e01955
Jialin Chen <jialinc7@mit.edu>
2025-03-17 00:01:02 -0400

    fixed oscillation

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -35,6 +35,9 @@ class ParkingController(Node):
         self.get_logger().info("Parking Controller Initialized")
 
 
+    # def look_around(self):
+    #     # modify the subscription to listen to ZED
+
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
         self.relative_y = msg.y_pos
@@ -54,65 +57,32 @@ class ParkingController(Node):
             # do the three point turn
             if dir == 'left':
                 drive_cmd.drive.steering_angle = -np.pi/2
-                drive_cmd.drive.speed = -0.75
+                drive_cmd.drive.speed = -0.5
             if dir == 'right':
                 drive_cmd.drive.steering_angle = np.pi/2
-                drive_cmd.drive.speed = -0.75
+                drive_cmd.drive.speed = -0.5
 
-        if self.distance_error > 0.1: # if too far from cone
-            # less than 90 deg turn, so turn directly towards cone
-            if angle_to_cone < abs(1.5):
+        if abs(angle_to_cone) < np.pi/3: # if angle less than 60 deg
+            if self.distance_error < 1 and abs(angle_to_cone) > 0.1: # too close, angle not reached, go back
+                drive_cmd.drive.steering_angle = -angle_to_cone
+                velocity = -0.8
+                drive_cmd.drive.speed = velocity
+                self.get_logger().info("1")
+            elif self.distance_error > 0.1 and abs(angle_to_cone) > 0.1: # too far, angle not reached, go forward
                 drive_cmd.drive.steering_angle = angle_to_cone
                 velocity = min(self.distance_error * 0.5, 0.8)
                 drive_cmd.drive.speed = velocity
+                self.get_logger().info("2")
             else:
-                if self.relative_y > 0: # on the left
-                    turn('left')
-                else:
-                    turn('right')
-        elif self.distance_error < -0.1: # if too close to clone
-            if self.relative_x < 0: # don't crash into cone backwards!
-                drive_cmd.drive.steering_angle = np.pi/2
-                velocity = -self.distance_error * 0.5 + 0.5
+                drive_cmd.drive.steering_angle = angle_to_cone
+                velocity = min(self.distance_error * 0.5, 0.8)
                 drive_cmd.drive.speed = velocity
-            else:
-                velocity = max(self.distance_error * 0.5, -0.5)
-                drive_cmd.drive.speed = velocity - 0.3 # move back extra so now case 1
+                self.get_logger().info("3")
         else:
-            if abs(angle_to_cone) > 0.1:
-                if self.relative_y > 0: # on the left
-                    turn('left')
-                else:
-                    turn('right')
-            else: drive_cmd.drive.speed = 0.0
-
-
-
-        #else:
-            # distance is good, do:
-            # Angle Check
-
-
-            # Get the closest wall point, do a 90 deg right or left turn away from it
-
-            # Move forward
-
-
-            # turn the opposite direction of the above but 45 deg
-
-
-            # move back
-
-
-            # turn 45 deg opposite to above
-
-
-            # move forward
-
-
-        # drive_cmd.drive.steering_angle = angle_to_cone
-        # drive_cmd.drive.speed = velocity
-        #################################
+            if self.relative_y > 0: # cone on the left
+                turn('left')
+            else:
+                turn('right')
 
         self.drive_pub.publish(drive_cmd)
         self.error_publisher()
